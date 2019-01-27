8f0d8da8118e9388c94b9c96e1d99d06290a8349
Mike Duin <michael.s.duin@gmail.com>
2019-01-27 00:47:28 -0800

    combine all team stat updating fns

--- modules/updateTeamStats.js
+++ modules/updateTeamStats.js
@@ -26,5 +26,17 @@ module.exports = {
     updateFullTeamBuild(10, 'teams_full_l10');
     updateFullTeamBuild(15, 'teams_full_l15');
     updateFullTeamBuild(20, 'teams_full_l20');
+  },
+  updateAllLineupBuilds: function() {
+    updatePartialTeamBuild(0, 'teams_starters', 'Starters');
+    updatePartialTeamBuild(5, 'teams_starters_l5', 'Starters');
+    updatePartialTeamBuild(10, 'teams_starters_l10', 'Starters');
+    updatePartialTeamBuild(15, 'teams_starters_l15', 'Starters');
+    updatePartialTeamBuild(20, 'teams_starters_l20', 'Starters');
+    updatePartialTeamBuild(0, 'teams_bench', 'Bench');
+    updatePartialTeamBuild(5, 'teams_bench_l5', 'Bench');
+    updatePartialTeamBuild(10, 'teams_bench_l10', 'Bench');
+    updatePartialTeamBuild(15, 'teams_bench_l15', 'Bench');
+    updatePartialTeamBuild(20, 'teams_bench_l20', 'Bench');
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,48 +2,26 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const knex = require('../db/knex');
-const dbBuilders = require('../modules/dbBuilders');
-const updateFullTeams = require('../modules/updateFullTeamBuilds');
 
+const dbBuilders = require('../modules/dbBuilders');
+const updateTeamStats = require('../modules/updateTeamStats');
 const advancedTeamStats = 'https://stats.nba.com/stats/leaguedashteamstats';
-// const advancedParams = (games) => {
-//   return {
-//     MeasureType: 'Advanced',
-//     PerMode: 'PerGame',
-//     PlusMinus: 'N',
-//     PaceAdjust: 'N',
-//     Rank: 'N',
-//     Season: '2018-19',
-//     SeasonType: 'Regular Season',
-//     Outcome: '',
-//     SeasonSegment: '',
-//     DateFrom: '',
-//     DateTo: '',
-//     OpponentTeamID: 0,
-//     VsConference: '',
-//     VsDivision: '',
-//     LastNGames: games,
-//     Location: '',
-//     Period: 0,
-//     GameSegment: '',
-//     Month: 0
-//   }
-// }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // updateFullTeams.updateAllFullTeamBuilds();
+  updateTeamStats.updateAllFullTeamBuilds();
+  updateTeamStats.updateAllLineupBuilds();
 
-  axios.get(advancedTeamStats, {
-      params: dbBuilders.fetchLineupParams(20, 'Bench')
-    })
-    .then((response)=> {
-      let teamData = response.data.resultSets[0].rowSet;
-      dbBuilders.buildTeamDb('teams_bench_l20', teamData);
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
+  // axios.get(advancedTeamStats, {
+  //     params: dbBuilders.fetchLineupParams(20, 'Bench')
+  //   })
+  //   .then((response)=> {
+  //     let teamData = response.data.resultSets[0].rowSet;
+  //     dbBuilders.buildTeamDb('teams_bench_l20', teamData);
+  //   })
+  //   .catch((err)=>{
+  //     console.log(err);
+  //   });
 
   res.render('index', { title: 'Express' });
 });
