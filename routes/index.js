f22208aebb1f267f23f958057c69a334308a15a4
Mike Duin <michael.s.duin@gmail.com>
2019-01-26 17:10:40 -0800

    add knex, full tm stat migration

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,34 @@ var router = express.Router();
 const axios = require('axios');
 
 const advancedTeamStats = 'https://stats.nba.com/stats/leaguedashteamstats';
+const advancedParams = (games) => {
+  return {
+    MeasureType: 'Advanced',
+    PerMode: 'PerGame',
+    PlusMinus: 'N',
+    PaceAdjust: 'N',
+    Rank: 'N',
+    Season: '2018-19',
+    SeasonType: 'Regular Season',
+    Outcome: '',
+    SeasonSegment: '',
+    DateFrom: '',
+    DateTo: '',
+    OpponentTeamID: 0,
+    VsConference: '',
+    VsDivision: '',
+    LastNGames: games,
+    Location: '',
+    Period: 0,
+    GameSegment: '',
+    Month: 0
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   axios.get(advancedTeamStats, {
-      params: {
-        MeasureType: 'Advanced',
-        PerMode: 'PerGame',
-        PlusMinus: 'N',
-        PaceAdjust: 'N',
-        Rank: 'N',
-        Season: '2018-19',
-        SeasonType: 'Regular Season',
-        Outcome: '',
-        SeasonSegment: '',
-        DateFrom: '',
-        DateTo: '',
-        OpponentTeamID: 0,
-        VsConference: '',
-        VsDivision: '',
-        LastNGames: 0,
-        Location: '',
-        Period: 0,
-        GameSegment: '',
-        Month: 0
-      }
+      params: advancedParams(0)
     })
     .then((response)=> {
       console.log(response.data);
