de26bcdf8738891724e44b695f124dfe2f92c80e
Mike Duin <michael.s.duin@gmail.com>
2019-01-28 04:03:40 -0800

    add all lineup net rtg mappers

--- modules/dbMappers.js
+++ modules/dbMappers.js
@@ -16,7 +16,7 @@ module.exports = {
               "*"
             )
             .then(res => {
-              console.log(res[0].team_name, " net rtg updated for full season");
+              console.log(res[0].team_name, " net rtg updated for FS");
             });
         });
       });
@@ -96,5 +96,195 @@ module.exports = {
             });
         });
       });
+
+    knex("teams_bench")
+      .select("team_id", "net_rating")
+      .then(teams => {
+        teams.forEach(team => {
+          knex("team_net_ratings")
+            .where({ team_id: team.team_id })
+            .update(
+              {
+                bench_full: team.net_rating,
+                updated_at: new Date()
+              },
+              "*"
+            )
+            .then(res => {
+              console.log(res[0].team_name, " net rtg updated for bench FS");
+            });
+        });
+      });
+
+    knex("teams_bench_l5")
+      .select("team_id", "net_rating")
+      .then(teams => {
+        teams.forEach(team => {
+          knex("team_net_ratings")
+            .where({ team_id: team.team_id })
+            .update(
+              {
+                bench_l5: team.net_rating,
+                updated_at: new Date()
+              },
+              "*"
+            )
+            .then(res => {
+              console.log(res[0].team_name, " net rtg updated for bench l5");
+            });
+        });
+      });
+
+    knex("teams_bench_l10")
+      .select("team_id", "net_rating")
+      .then(teams => {
+        teams.forEach(team => {
+          knex("team_net_ratings")
+            .where({ team_id: team.team_id })
+            .update(
+              {
+                bench_l10: team.net_rating,
+                updated_at: new Date()
+              },
+              "*"
+            )
+            .then(res => {
+              console.log(res[0].team_name, " net rtg updated for bench l10");
+            });
+        });
+      });
+
+    knex("teams_bench_l15")
+      .select("team_id", "net_rating")
+      .then(teams => {
+        teams.forEach(team => {
+          knex("team_net_ratings")
+            .where({ team_id: team.team_id })
+            .update(
+              {
+                bench_l15: team.net_rating,
+                updated_at: new Date()
+              },
+              "*"
+            )
+            .then(res => {
+              console.log(res[0].team_name, " net rtg updated for bench l15");
+            });
+        });
+      });
+
+    knex("teams_bench_l20")
+      .select("team_id", "net_rating")
+      .then(teams => {
+        teams.forEach(team => {
+          knex("team_net_ratings")
+            .where({ team_id: team.team_id })
+            .update(
+              {
+                bench_l20: team.net_rating,
+                updated_at: new Date()
+              },
+              "*"
+            )
+            .then(res => {
+              console.log(res[0].team_name, " net rtg updated for bench l20");
+            });
+        });
+      });
+
+      knex("teams_starters")
+        .select("team_id", "net_rating")
+        .then(teams => {
+          teams.forEach(team => {
+            knex("team_net_ratings")
+              .where({ team_id: team.team_id })
+              .update(
+                {
+                  starters_full: team.net_rating,
+                  updated_at: new Date()
+                },
+                "*"
+              )
+              .then(res => {
+                console.log(res[0].team_name, " net rtg updated for starters FS");
+              });
+          });
+        });
+
+      knex("teams_starters_l5")
+        .select("team_id", "net_rating")
+        .then(teams => {
+          teams.forEach(team => {
+            knex("team_net_ratings")
+              .where({ team_id: team.team_id })
+              .update(
+                {
+                  starters_l5: team.net_rating,
+                  updated_at: new Date()
+                },
+                "*"
+              )
+              .then(res => {
+                console.log(res[0].team_name, " net rtg updated for starters l5");
+              });
+          });
+        });
+
+      knex("teams_starters_l10")
+        .select("team_id", "net_rating")
+        .then(teams => {
+          teams.forEach(team => {
+            knex("team_net_ratings")
+              .where({ team_id: team.team_id })
+              .update(
+                {
+                  starters_l10: team.net_rating,
+                  updated_at: new Date()
+                },
+                "*"
+              )
+              .then(res => {
+                console.log(res[0].team_name, " net rtg updated for starters l10");
+              });
+          });
+        });
+
+      knex("teams_starters_l15")
+        .select("team_id", "net_rating")
+        .then(teams => {
+          teams.forEach(team => {
+            knex("team_net_ratings")
+              .where({ team_id: team.team_id })
+              .update(
+                {
+                  starters_l15: team.net_rating,
+                  updated_at: new Date()
+                },
+                "*"
+              )
+              .then(res => {
+                console.log(res[0].team_name, " net rtg updated for starters l15");
+              });
+          });
+        });
+
+      knex("teams_starters_l20")
+        .select("team_id", "net_rating")
+        .then(teams => {
+          teams.forEach(team => {
+            knex("team_net_ratings")
+              .where({ team_id: team.team_id })
+              .update(
+                {
+                  starters_l20: team.net_rating,
+                  updated_at: new Date()
+                },
+                "*"
+              )
+              .then(res => {
+                console.log(res[0].team_name, " net rtg updated for starters l20");
+              });
+          });
+        });
   }
 };
