7b53fffddc21ec339b50e87a763f666e71a2c900
Mike Duin <michael.s.duin@gmail.com>
2019-02-02 02:51:23 -0800

    load FG net ratings table

--- client/src/components/NetRatings.js
+++ client/src/components/NetRatings.js
@@ -19,29 +19,110 @@ class NetRatings extends React.Component {
         <ReactTable
           data={this.props.netRatings}
           defaultPageSize={30}
+          pageSizeOptions={[30]}
           columns={[
             {
               Header: 'Team',
-              accessor: 'team_name'
+              accessor: 'team_name',
+              minWidth: 150
             },
             {
               Header: 'Full Roster',
               columns: [
                 {
                   Header: 'Season',
-                  accessor: 'team_full'
+                  accessor: 'team_full',
+                  maxWidth: 60
                 },
                 {
                   Header: 'L5',
-                  accessor: 'team_l5'
+                  accessor: 'team_l5',
+                  maxWidth: 60
                 },
                 {
                   Header: 'L10',
-                  accessor: 'team_l10'
+                  accessor: 'team_l10',
+                  maxWidth: 60
                 },
                 {
                   Header: 'L15',
-                  accessor: 'team_l15'
+                  accessor: 'team_l15',
+                  maxWidth: 60
+                }
+              ]
+            },
+            {
+              Header: 'Starters',
+              columns: [
+                {
+                  Header: 'Season',
+                  accessor: 'starters_full',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L5',
+                  accessor: 'starters_l5',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L10',
+                  accessor: 'starters_l10',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L15',
+                  accessor: 'starters_l15',
+                  maxWidth: 60
+                }
+              ]
+            },
+            {
+              Header: 'Bench',
+              columns: [
+                {
+                  Header: 'Season',
+                  accessor: 'bench_full',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L5',
+                  accessor: 'bench_l5',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L10',
+                  accessor: 'bench_l10',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L15',
+                  accessor: 'bench_l15',
+                  maxWidth: 60
+                }
+              ]
+            },
+            {
+              Header: 'Bench vs. Starters',
+              columns: [
+                {
+                  Header: 'Season',
+                  accessor: 'bs_delta_full',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L5',
+                  accessor: 'bs_delta_l5',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L10',
+                  accessor: 'bs_delta_l10',
+                  maxWidth: 60
+                },
+                {
+                  Header: 'L15',
+                  accessor: 'bs_delta_l15',
+                  maxWidth: 60
                 }
               ]
             }
