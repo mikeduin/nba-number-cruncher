c3c3d0a0ec8589d1a431d38af2b6bf5a75f127c7
Mike Duin <michael.s.duin@gmail.com>
2019-03-12 02:21:29 -0700

    add teams_full_base migration:

---
+++ migrations/20190312020309_teams_full_base.js
@@ -0,0 +1,54 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('teams_full_base', (t) => {
+    t.increments();
+    t.integer('team_id');
+    t.string('team_name');
+    t.float('min');
+    t.float('fgm');
+    t.float('fga');
+    t.float('fg_pct');
+    t.float('fg3m');
+    t.float('fg3a');
+    t.float('fg3_pct');
+    t.float('ftm');
+    t.float('fta');
+    t.float('oreb');
+    t.float('dreb');
+    t.float('reb');
+    t.float('ast');
+    t.float('tov');
+    t.float('stl');
+    t.float('blk');
+    t.float('blka');
+    t.float('pf');
+    t.float('pfd');
+    t.float('pts');
+    t.float('plus_minus');
+    t.integer('gp_rank');
+    t.integer('w_rank');
+    t.integer('l_rank');
+    t.integer('win_pct_rank');
+    t.integer('min_rank');
+    t.integer('fgm_rank');
+    t.integer('fga_rank');
+    t.integer('fg_pct_rank');
+    t.integer('oreb_rank');
+    t.integer('dreb_rank');
+    t.integer('reb_rank');
+    t.integer('ast_rank');
+    t.integer('tov_rank');
+    t.integer('stl_rank');
+    t.integer('blk_rank');
+    t.integer('blka_rank');
+    t.integer('pf_rank');
+    t.integer('pfd_rank');
+    t.integer('pts_rank');
+    t.integer('plus_minus_rank');
+    t.integer('cfid');
+    t.string('cfparams');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('teams_full_base')
+};
