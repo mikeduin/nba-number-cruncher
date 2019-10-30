e8520a09314720d5495200705c41fc2dc4106170
Mike Duin <michael.s.duin@gmail.com>
2019-10-29 19:30:11 -0700

    add prod env to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,10 @@ module.exports = {
   development: {
     client: 'postgresql',
     // connection: 'postgres://mikeduin620:kennysmith30@nbanumbercruncher.chdu4cpdyavi.us-east-1.rds.amazonaws.com:5432/nba_number_cruncher'
-    connection: 'postgres://localhost/nba-number-cruncher'
+    connection: process.env.DATABASE_URL
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 };
