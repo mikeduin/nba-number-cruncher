1b4ea0e876cf4077c65c3b300fa5a23fcd33cca9
Mike Duin <michael.s.duin@gmail.com>
2019-11-07 12:05:13 -0800

    change node scheduler

--- routes/index.js
+++ routes/index.js
@@ -36,8 +36,8 @@ let rule = new schedule.RecurrenceRule();
 rule.tz = 'America/Los_Angeles';
 
 // test values
-rule.hour = 1;
-rule.minute = 35;
+rule.hour = 12;
+rule.minute = 10;
 rule.second = 0;
 
 const timedDbUpdaters = schedule.scheduleJob(rule, () => {
