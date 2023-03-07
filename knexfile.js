2baae0c705ed017998fe20b7305617292b99b84c
Michael Duin <michael.s.duin@gmail.com>
2023-03-07 05:37:39 -0800

    try diff knex approach

--- knexfile.js
+++ knexfile.js
@@ -3,25 +3,54 @@
 //    rejectUnauthorized: false,
 // }
 
+// require('dotenv').config();
+// const parse = require("pg-connection-string").parse;
+
+// // const pgconfig = parse(process.env.MAIN_DB_URI);
+// const pgconfig = parse(process.env.DATABASE_URL_TEST_NEW);
+// // const pgconfig = parse(process.env.TEST_MAIN_DB_2);
+
+// // Add SSL setting to default environment variable
+// pgconfig.ssl = { rejectUnauthorized: false };
+
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     // connection: process.env.DATABASE_URL
+//     connection: pgconfig
+//   },
+//   production: {
+//     client: 'postgresql',
+//     connection: process.env.DATABASE_URL
+//     // connection: process.env.DATABASE_URL_TEST_NEW
+//   }
+// };
+
 require('dotenv').config();
-const parse = require("pg-connection-string").parse;
+const pg = require('pg')
+pg.defaults.ssl = {
+   rejectUnauthorized: false,
+}
 
-// const pgconfig = parse(process.env.MAIN_DB_URI);
-const pgconfig = parse(process.env.DATABASE_URL_TEST_NEW);
-// const pgconfig = parse(process.env.TEST_MAIN_DB_2);
+// const parse = require("pg-connection-string").parse;
+// const pgconfig = parse(process.env.TEST_DB_URL);
 
-// Add SSL setting to default environment variable
-pgconfig.ssl = { rejectUnauthorized: false };
+// console.log(process.env);
 
 module.exports = {
   development: {
     client: 'pg',
-    // connection: process.env.DATABASE_URL
-    connection: pgconfig
+    // connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL_TEST_NEW,
+    // connection: pgconfig,
+    ssl: {
+      rejectUnauthorized: false,  
+    },
+    ssl: 'no-verify' // try this if above does not work for you
   },
+
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL
-    // connection: process.env.DATABASE_URL_TEST_NEW
   }
 };
