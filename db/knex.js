3329165baa5b19feb97a5e4a1c4e2464ac64256f
Michael Duin <michaelduin@Michaels-MBP-2.lan>
2024-10-06 19:58:47 -0700

    fix require statement

--- db/knex.js
+++ db/knex.js
@@ -1,7 +1,3 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const config = require('../knexfile.js')[environment];
-// module.exports = require('knex')(config);
-
 import knex from 'knex';
 import { config as dotenvConfig } from 'dotenv';
 import knexfile from '../knexfile.js';
